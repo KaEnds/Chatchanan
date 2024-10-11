ddf3c95b7f2d2725710a77c7af9635c203314746
KaEnds <kenza.ke15@gmail.com>
2024-10-11 16:55:41 +0700

    add project page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import App from './App.jsx'
+import Nawintrade from './Components/Nawintrade.jsx'
+import Caraball from './Components/Caraball.jsx';
+import Califonia from './Components/Califonia.jsx';
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {path: "/Chatchanan", element: <App />,},
+  {path: "/Chatchanan/project/nawintrade", element: <Nawintrade />},
+  {path: "/Chatchanan/project/Caraball", element: <Caraball />},
+  {path: "/Chatchanan/project/Califonia", element: <Califonia />},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
