9a9de24b9b0f4781ad3bd233e459075e8b686446
KaEnds <kenza.ke15@gmail.com>
2024-10-11 14:30:09 +0700

    test change

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>hi this is first change of deploy</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
